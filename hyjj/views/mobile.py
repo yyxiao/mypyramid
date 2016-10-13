3c932c2e969344ff3d91cf94f61ec1b6a4700f2d
xyy <xiaoyangyang@moseeker.com>
2016-10-13 15:53:36 +0800

    add code redis exprie

--- hyjj/views/mobile.py
+++ hyjj/views/mobile.py
@@ -4,8 +4,9 @@
 __author__ = xyy
 __mtime__ = 2016/10/13
 """
+
 from pyramid.view import view_config
-from ..service.sendsms_service import sendSmsService
+from ..service.sendsms_service import add_sms, add_code_redis
 from ..common.sendsms import send
 from ..common.constant import CODE_ERROR, CODE_SUCCESS, CODE_WRONG
 
@@ -17,7 +18,7 @@ def send_code(request):
     :param request:
     :return:
     """
-    self = request.dbsession
+    dbs = request.dbsession
     user_id = request.POST.get('user_id', '')
     user_phone = request.POST.get('phone', 0)
     user_name = request.POST.get('name', '')
@@ -28,7 +29,7 @@ def send_code(request):
         error_msg = '用户姓名不能为空！'
     if not error_msg:
         send(user_phone, content % (user_name, '111111'))
-        error_msg = sendSmsService.add_sms(self, sms_content=content, phone=user_phone, create_user=user_id)
+        error_msg = add_sms(dbs, sms_content=content, phone=user_phone, create_user=user_id)
     if error_msg:
         json_a = {
             'returnCode': CODE_ERROR,
@@ -42,15 +43,8 @@ def send_code(request):
     return json_a
 
 
-@view_config(route_name='sendCode', renderer='json')
-def send_code(request):
-    """
-    设备初始化登录
-    :param request:
-    :return:
-    """
-    self = request.dbsession
-    request.session['12'] = '122'
-    request.session['13'] = '133'
-    request.session['14'] = '144'
-    request.session['14']['timeout'] = 10
+@view_config(route_name='test123', renderer='json')
+def send_test(request):
+    redis_host = request.registry.settings['redis.sessions.host']
+    add_code_redis(15800786806, 201293, redis_host)
+    add_code_redis(15800786807, 201294, redis_host)
