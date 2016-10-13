63ca672307fdec381eb15a2663c8e69e4ca1766d
xyy <xyy-JAVA@outlook.com>
2016-10-13 15:53:36 +0800

    add code redis exprie

--- hyjj/service/sendsms_service.py
+++ hyjj/service/sendsms_service.py
@@ -5,21 +5,28 @@ __author__ = xyy
 __mtime__ = 2016/10/13
 """
 
+import redis
 from ..models.model import CustomerSms
-from ..common.constant import STATE_INVALID, STATE_VALID
+from ..common.constant import STATE_INVALID, STATE_VALID, TIMEOUT_CODE
 
 
-class sendSmsService:
-    @staticmethod
-    def add_sms(dbs, sms_content, phone, create_user):
-        msg = ''
-        try:
-            sms = CustomerSms()
-            sms.sms_content = sms_content
-            sms.phone = phone
-            sms.create_user = create_user
-            sms.state = STATE_VALID
-            dbs.add(sms)
-        except Exception as e:
-            msg = '新增短信发送记录失败，请核对后重试'
-        return msg
+def add_sms(dbs, sms_content, phone, create_user):
+    msg = ''
+    try:
+        sms = CustomerSms()
+        sms.sms_content = sms_content
+        sms.phone = phone
+        sms.create_user = create_user
+        sms.state = STATE_VALID
+        dbs.add(sms)
+    except Exception as e:
+        msg = '新增短信发送记录失败，请核对后重试'
+    return msg
+
+
+def add_code_redis(phone, code, redis_host):
+    pool = redis.ConnectionPool(host=redis_host, port=6379, db=0)
+    r = redis.StrictRedis(connection_pool=pool)
+    r.set(phone, code)
+    # 设置过期失效时间
+    r.expire(phone, TIMEOUT_CODE)
