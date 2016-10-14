8ea9b13278f7e095445d03afcadf9ac69e15c398
xyy <xyy-JAVA@outlook.com>
2016-10-14 18:16:28 +0800

    add all

--- hyjj/common/base.py
+++ hyjj/common/base.py
@@ -5,11 +5,15 @@ __author__ = xyy
 __mtime__ = 2016/10/14
 """
 import importlib
+from ..service import sendsms_service
+
 
 class BaseUtil:
 
     def __init__(self, request):
         self.request = request
 
-        customer_mod = importlib.import_module('service.{0}'.format('customer_service'))
+        customer_mod = importlib.import_module('hyjj.service.{0}'.format('customer_service'))
+        hy_log = importlib.import_module('hyjj.common.{0}'.format('loguntil'))
         self.customerService = getattr(customer_mod, 'CustomerService')()
+        self.hyLog = getattr(hy_log, 'HyLog')()
