de88d1c8b26803539416b9ac06199d7e76abdd2f
xyy <xiaoyangyang@moseeker.com>
2016-10-14 10:42:49 +0800

    add json_response

--- hyjj/views/mobile.py
+++ hyjj/views/mobile.py
@@ -6,9 +6,12 @@ __mtime__ = 2016/10/13
 """
 
 from pyramid.view import view_config
+from datetime import datetime
 from ..service.sendsms_service import add_sms, add_code_redis, make_random
 from ..common.sendsms import send
 from ..common.constant import CODE_ERROR, CODE_SUCCESS, CODE_WRONG
+from ..common.loguntil import HyLog
+from ..common.jsonutils import other_response
 
 
 @view_config(route_name='sendCode', renderer='json')
@@ -20,7 +23,6 @@ def send_code(request):
     """
     error_msg = ''
     dbs = request.dbsession
-    user_id = request.POST.get('user_id', '')
     user_phone = request.POST.get('phone', '')
     user_name = request.POST.get('name', '')
     content = '尊敬的%s先生，您好，您网上交易重置登录密码验证码为%s，请在三分钟内输入该有效验证码'
@@ -34,7 +36,7 @@ def send_code(request):
         send(user_phone, content)
         redis_host = request.registry.settings['redis.sessions.host']
         add_code_redis(user_phone, code, redis_host)
-        error_msg = add_sms(dbs, sms_content=content, phone=user_phone, create_user=user_id)
+        error_msg = add_sms(dbs, sms_content=content, phone=user_phone)
     if error_msg:
         json_a = {
             'returnCode': CODE_ERROR,
@@ -45,11 +47,17 @@ def send_code(request):
             'returnCode': CODE_SUCCESS,
             'returnMsg': ''
         }
-    return json_a
+    HyLog.log_in(request.client_addr, '', ('sendCode failed ' + error_msg if error_msg else 'sendCode success'),
+                 'mobile')
+    resp = other_response(json_a=json_a)
+    return resp
 
 
-@view_config(route_name='test123', renderer='json')
+@view_config(route_name='test', renderer='json')
 def send_test(request):
     redis_host = request.registry.settings['redis.sessions.host']
+    date1 = datetime.now().strftime('%Y-%m-%d 23:59:59 %f')
+    date2 = datetime.now().strftime('%Y-%m-%d 23:59:59 %f')
+    date3 = datetime.now().strftime('%Y-%m-%d 23:59:59 %f')
     add_code_redis(15800786806, 201293, redis_host)
     # add_code_redis(15800786807, 201294, redis_host)
