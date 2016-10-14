6f0827b06ce9b11f9d67b7b486ec4caddddb9b3e
xyy <xiaoyangyang@moseeker.com>
2016-10-14 17:00:15 +0800

    update base_handler

--- hyjj/common/base.py
+++ hyjj/common/base.py
@@ -4,9 +4,12 @@
 __author__ = xyy
 __mtime__ = 2016/10/14
 """
-
+import importlib
 
 class BaseUtil:
 
     def __init__(self, request):
         self.request = request
+
+        customer_mod = importlib.import_module('service.{0}'.format('customer_service'))
+        self.customerService = getattr(customer_mod, 'CustomerService')()
